7cac0de612e95f7e4127f1005c89b5ab3f51cc8d
Miracle5222 <roneilgungobbansas5222@gmail.com>
2022-07-06 03:46:45 -0700

    second commit, adding redux

--- App.js
+++ App.js
@@ -1,61 +1,48 @@
-import * as React from 'react';
-import { View, Text,StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LandingScreen from './screens/LandingScreen';
-import { ThemeProvider, Button, createTheme } from '@rneui/themed';
+import * as React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LandingScreen from "./screens/LandingScreen";
+import { ThemeProvider, Button, createTheme } from "@rneui/themed";
 
 const theme = createTheme({
   Button: {
     raised: true,
   },
-  Bg:{
-    primary:'#0D0D0D',
+  Bg: {
+    primary: "#0D0D0D",
   },
-  Fontsize:{
-    large:46,
-    medium:32,
-    small:18,
-    extraSmall:12
+  Fontsize: {
+    large: 46,
+    medium: 32,
+    small: 18,
+    extraSmall: 12,
   },
-  color:{
-    orange:'#FF7700',
-  }
+  color: {
+    orange: "#FF7700",
+  },
 });
-
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-      <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
       <NavigationContainer>
-            <Stack.Navigator screenOptions={{ headerShown: false}}>
-              <Stack.Screen name="LandingScreen" component={LandingScreen} />
-            </Stack.Navigator>
-          </NavigationContainer>
-      </ThemeProvider>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="LandingScreen" component={LandingScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
 export default App;
 
-
-const styled = StyleSheet.compose({
-
-container: {
-  flex:1,
-  justifyContent: 'center',
-  alignItems: 'center',
-
-}
-
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- screens/LandingScreen.js
+++ screens/LandingScreen.js
@@ -1,43 +1,49 @@
-import { StyleSheet, Text, View,Button } from 'react-native'
-import React from 'react'
-import { useTheme } from '@rneui/themed';
-import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from "react-native";
+import React from "react";
+import { useTheme } from "@rneui/themed";
+import { StatusBar } from "expo-status-bar";
 
-const LandingScreen = ({navigation}) => {
-    const {theme} = useTheme();
-    return (
-        <>
-        <StatusBar style="light" />
-        <View style={[styles.container, {backgroundColor: theme.Bg.primary}]}>
-            <View>
-                <Text style={[styles.textHeader,{fontSize:theme.Fontsize.large}]}>Basic Programming E-learning Application</Text>
-                <Text style={[styles.textParagraph,{fontSize:theme.Fontsize.small}]}>The best app to learn Programming</Text>
-            </View>
-            <View>
-                <Button title="SIGN-IN" style={}/>
-            </View>
+const LandingScreen = ({ navigation }) => {
+  const { theme } = useTheme();
+  return (
+    <>
+      <StatusBar style="light" />
+      <View style={[styles.container, { backgroundColor: theme.Bg.primary }]}>
+        <View style={styles.headerContainer}>
+          <Text style={[styles.textHeader, { fontSize: theme.Fontsize.large }]}>
+            Basic Programming E-learning Application
+          </Text>
+          <Text
+            style={[styles.textParagraph, { fontSize: theme.Fontsize.small }]}
+          >
+            The best app to learn Programming
+          </Text>
         </View>
-        </>
-    )
-}
+        <View>
+          <Button title="SIGN-IN" />
+        </View>
+      </View>
+    </>
+  );
+};
 
-export default LandingScreen
+export default LandingScreen;
 
-const styles = StyleSheet.compose({
-
-    container: {
-      flex:1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    textHeader:{
-        color: 'white',
-        textAlign: 'left',
-    },
-    textParagraph:{
-        color: '#ffffff',
-        fontWeight:'100',
-        textAlign: 'left',
-    } 
-    
-})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerContainer: {
+    textAlign: "center",
+  },
+  textHeader: {
+    color: "white",
+    textAlign: "left",
+  },
+  textParagraph: {
+    color: "#ffffff",
+    fontWeight: "100",
+    textAlign: "left",
+  },
+  
+});
